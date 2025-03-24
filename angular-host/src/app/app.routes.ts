9e245c3013c9638f5f86358492bb144783000fdf
mattiaTuri <turina.mattia@gmail.com>
2025-03-24 23:05:41 +0100

    added native federation

--- angular-host/src/app/app.routes.ts
+++ angular-host/src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'angular-service',
+    loadComponent: () =>
+      loadRemoteModule('angular-service', './Component').then(
+        (m) => m.AppComponent
+      ),
+  },
+];
